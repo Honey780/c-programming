23b4c8a1578405302edb1a2029977741de58fe51
Honey780 <dabassohoney78@gmail.com>
2024-05-21 16:37:37 +0300

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello World");
+
+    return 0;
+}
+
+
+
+/tmp/eSNkgpEBxK.o
+Hello World
+
+=== Code Execution Successful ===
